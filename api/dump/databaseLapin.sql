de70e749f9eb9999b7546efa25a98b3e46905d5b
Quentin <quentinpourriot@outlook.fr>
2017-02-11 10:17:47 +0100

    modify database

--- api/dump/databaseLapin.sql
+++ api/dump/databaseLapin.sql
@@ -51,8 +51,7 @@ CREATE TABLE `info` (
   `short_name` varchar(255) DEFAULT NULL,
   `large_name` varchar(255) DEFAULT NULL,
   `author` varchar(255) DEFAULT NULL,
-  `favicon` mediumtext,
-  `favicon_name` varchar(250) DEFAULT NULL,
+  `favicon` BLOB,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
